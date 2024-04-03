ad77ffc8d1003873f928db0d98740cf4fe48bd2f
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-04-03 10:36:21 +0530

    feat: routing

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './assets/main.css'
+import router from './router'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store'
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center justify-center p-8">
+    <input
+      type="text"
+      class="text-black rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    />
+    <div class="flex gap-1"></div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import store from '../store'
+
+const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+</script>
